41e4902be500af1b970cf3ed7f7f32dc8db81179
nastuaah <homenko.nastya2004@yandex.ru>
2023-05-14 22:57:47 +0300

    activity_start.xml, QuizActivity.java

--- app/src/main/java/com/example/testingsystemproject/question_activities/StartActivity.java
+++ app/src/main/java/com/example/testingsystemproject/question_activities/StartActivity.java
@@ -1,4 +1,11 @@
 package com.example.testingsystemproject.question_activities;
 
+import android.content.Intent;
+import android.view.View;
+
+import com.example.testingsystemproject.QuizActivity;
+
+
 public class StartActivity {
+
 }
