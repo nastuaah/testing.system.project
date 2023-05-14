33810b7872e77e8346d429eb48f8d158642783af
polinabog475@gmail.com <polinabog475@gmail.com>
2023-05-14 16:55:49 +0300

    UserRepositoryoty

--- app/src/main/java/com/example/testingsystemproject/repositories/UserRepository.java
+++ app/src/main/java/com/example/testingsystemproject/repositories/UserRepository.java
@@ -1,6 +1,11 @@
 package com.example.testingsystemproject.repositories;
 
+import com.example.testingsystemproject.models.User;
+
 public class UserRepository {
     public boolean checkIfUserWithEmailExists(String toString) {
+    }
+
+    public void insert(User user) {
     }
 }
