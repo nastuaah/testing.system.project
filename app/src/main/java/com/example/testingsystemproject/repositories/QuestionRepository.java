9c4f14bafefa45852646357f6887943d772409a6
polinabog475@gmail.com <polinabog475@gmail.com>
2023-05-14 18:37:39 +0300

    QuestionRepository and SecurityQuestionRepositoryQuest

--- app/src/main/java/com/example/testingsystemproject/repositories/QuestionRepository.java
+++ app/src/main/java/com/example/testingsystemproject/repositories/QuestionRepository.java
@@ -1,4 +1,12 @@
 package com.example.testingsystemproject.repositories;
+import com.example.testingsystemproject.dao.QuestionDao;
+import com.example.testingsystemproject.database.AppDatabase;
+import javax.inject.Singleton;
 
+@Singleton
 public class QuestionRepository {
+    private final QuestionDao dao;
+    public QuestionRepository(AppDatabase appDatabase) {
+        dao = appDatabase.questionDao();
+    }
 }
--- app/src/main/java/com/example/testingsystemproject/repositories/SecurityQuestionRepository.java
+++ app/src/main/java/com/example/testingsystemproject/repositories/SecurityQuestionRepository.java
@@ -1,4 +1,18 @@
 package com.example.testingsystemproject.repositories;
 
+import com.example.testing_system.dao.SecurityQuestionDao;
+import com.example.testing_system.database.AppDatabase;
+import java.util.List;
+import javax.inject.Singleton;
+
+@Singleton
 public class SecurityQuestionRepository {
+    private final SecurityQuestionDao dao;
+    public SecurityQuestionRepository(AppDatabase appDatabase) {
+        dao = appDatabase.securityQuestionDao();
+    }
+
+    public List<String> getAllNames() {
+        return dao.getAllNames();
+    }
 }
