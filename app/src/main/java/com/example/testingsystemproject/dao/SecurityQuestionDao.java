5b6249b496b598775ead58c23fc68458146728c8
polinabog475@gmail.com <polinabog475@gmail.com>
2023-05-14 16:37:14 +0300

    SecurityQuestionDao

---
+++ app/src/main/java/com/example/testingsystemproject/dao/SecurityQuestionDao.java
@@ -0,0 +1,19 @@
+package com.example.testingsystemproject.dao;
+
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.Query;
+
+import java.util.List;
+
+@Dao
+public interface SecurityQuestionDao {
+    @Query("SELECT question from SecurityQuestion")
+    List<String> getAllNames();
+
+    @Query("SELECT CASE WHEN EXISTS(SELECT 1 FROM SecurityQuestion) THEN 1 ELSE 0 END")
+    boolean any();
+
+    @Insert
+    void insert(SecurityQuestion... securityQuestions);
+}
