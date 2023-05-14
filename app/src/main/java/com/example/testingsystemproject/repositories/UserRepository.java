94f48f8c09cb99b388167bd792c020922f820a5a
polinabog475@gmail.com <polinabog475@gmail.com>
2023-05-14 18:38:40 +0300

    UserRepository

--- app/src/main/java/com/example/testingsystemproject/repositories/UserRepository.java
+++ app/src/main/java/com/example/testingsystemproject/repositories/UserRepository.java
@@ -2,11 +2,30 @@ package com.example.testingsystemproject.repositories;
 
 import com.example.testingsystemproject.models.User;
 
-public class UserRepository {
-    public boolean checkIfUserWithEmailExists(String toString) {
+import com.example.testingsystemproject.dao.UserDao;
+import com.example.testingsystemproject.database.AppDatabase;
+import com.example.testingsystemproject.helpers.EncryptionHelper;
+import com.example.testingsystemproject.models.User;
+import java.util.Objects;
+import javax.inject.Singleton;
 
+@Singleton
+public class UserRepository {
+    private final UserDao userDao;
+
+    public UserRepository(AppDatabase appDatabase) {
+        this.userDao = appDatabase.userDao();
+    }
+    public boolean checkIfUserWithEmailExists(String email) {
+        return userDao.checkIfUserWithEmailExists(email);
     }
 
     public void insert(User user) {
+        userDao.insert(user);
+    }
+
+    public boolean authenticate(String userName, String password) {
+        String passwordHashFromDatabase = userDao.getPasswordByUserName(userName);
+        return Objects.equals(passwordHashFromDatabase, EncryptionHelper.toSHA256String(password));
     }
 }
