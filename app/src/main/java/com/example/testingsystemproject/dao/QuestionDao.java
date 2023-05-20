4b24834edab681d75239d48a2bb21264facd1c5d
nastuaah <homenko.nastya2004@yandex.ru>
2023-05-20 14:47:02 +0300

    QuizActivity.java started, comments in QuestionDao.java, methods should be added

--- app/src/main/java/com/example/testingsystemproject/dao/QuestionDao.java
+++ app/src/main/java/com/example/testingsystemproject/dao/QuestionDao.java
@@ -2,10 +2,24 @@ package com.example.testingsystemproject.dao;
 
 import androidx.room.Dao;
 import androidx.room.Insert;
+import androidx.room.Query;
+
+import com.example.testingsystemproject.models.Category;
 import com.example.testingsystemproject.models.Question;
+
+import java.util.List;
 
 @Dao
 public interface QuestionDao {
     @Insert
     void insert(Question... questions);
+    //@Query("SELECT * from question WHERE category_id=:")
+    //List<Question> getAll();
+    //Тань, сделай, пожалуйста, вызов вопросов по категории, чтобы вызывалось пять вопросов для
+    //Квиз активити
+
+    //метод getQuestion, чтобы вопросы вызывались по очереди, вопросы для текстового окна
+    // getOption1, getOption2, getOption3 ответы для каждой кнопки опций
+    //нужно еще две категории вставить
+    //метод getAnswer
 }
