b883d53c5a767f342aa5bcc6329b55327d103dfb
SofiaLumbik <lumbik04@mail.ru>
2023-05-22 19:53:18 +0300

    5 категория

--- app/src/main/java/com/example/testingsystemproject/dao/QuestionDao.java
+++ app/src/main/java/com/example/testingsystemproject/dao/QuestionDao.java
@@ -13,18 +13,8 @@ import java.util.List;
 public interface QuestionDao {
     @Insert
     void insert(Question... questions);
-    @Query(" SELECT * from question " + " INNER JOIN category ON category.categoryId = category_id " + " WHERE questionId = :id ")
-    Question getByQuestion(int id);
-    @Query(" SELECT * from question WHERE option1 = :opt1 AND questionId = :id")
-    String getByOption1(String opt1, int id);
-    @Query("SELECT * from question WHERE option2 = :opt2 AND questionId = :id")
-    String getByOption2(String opt2, int id);
-    @Query("SELECT * from question WHERE option3 = :opt3 AND questionId = :id")
-    String getByOption3(String opt3, int id);
-    @Query("SELECT * from question WHERE answer = :answer AND questionId = :id")
-    String getByAnswer(String answer, int id);
     //@Query("SELECT * from question WHERE category_id=:")
-    //List<Question> getAll(int id);
+    //List<Question> getAll();
     //Тань, сделай, пожалуйста, вызов вопросов по категории, чтобы вызывалось пять вопросов для
     //Квиз активити
 
