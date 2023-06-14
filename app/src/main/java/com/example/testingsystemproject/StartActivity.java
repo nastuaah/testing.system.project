06758ff1ce50f82bb42a6c1f831b52b1f8d34db0
nastuaah <homenko.nastya2004@yandex.ru>
2023-06-14 17:26:40 +0300

    StartActivity.java, activity_start.xml changed

--- app/src/main/java/com/example/testingsystemproject/StartActivity.java
+++ app/src/main/java/com/example/testingsystemproject/StartActivity.java
@@ -16,8 +16,8 @@ public class StartActivity extends AppCompatActivity {
         Intent myIntent = new Intent(this, AccountActivity.class);
         startActivity(myIntent);
     }
-    public void RedirectToQuiz(View view) {
-        Intent myIntent = new Intent(this, QuizActivity.class);
+    public void RedirectToOptionActivity(View view) {
+        Intent myIntent = new Intent(this, OptionActivity.class);
         startActivity(myIntent);
     }
 }
