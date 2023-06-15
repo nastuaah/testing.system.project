2a27837df7e012a63327e5635b709d4fc7ac7e80
nastuaah <homenko.nastya2004@yandex.ru>
2023-06-15 19:24:25 +0300

    TestResult.java changed

--- app/src/main/java/com/example/testingsystemproject/dtos/TestResult.java
+++ app/src/main/java/com/example/testingsystemproject/dtos/TestResult.java
@@ -3,11 +3,13 @@ package com.example.testingsystemproject.dtos;
 import java.util.List;
 
 public class TestResult {
+    private final long categoryId;
     private final long userId;
     private final List<Long> answerId;
     private final List<Long> questionId;
 
-    public TestResult(long userId, List<Long> answerId, List<Long> questionId) {
+    public TestResult(long categoryId, long userId, List<Long> answerId, List<Long> questionId) {
+        this.categoryId = categoryId;
         this.userId = userId;
         this.answerId = answerId;
         this.questionId = questionId;
@@ -23,5 +25,9 @@ public class TestResult {
 
     public List<Long> getQuestionId() {
         return questionId;
+    }
+
+    public long getCategoryId() {
+        return categoryId;
     }
 }
